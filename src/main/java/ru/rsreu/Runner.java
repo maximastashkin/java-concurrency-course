9207fcc27d55424dbdd8ac515c8f20f75dc80cfa
maximastashkin <maksim.astash@gmail.com>
2022-09-19 16:07:29 +0300

    Time fix

--- src/main/java/ru/rsreu/Runner.java
+++ src/main/java/ru/rsreu/Runner.java
@@ -5,11 +5,11 @@ import ru.rsreu.calculation.RectangleMethodIntegralCalculator;
 public class Runner {
     public static void main(String[] args) throws InterruptedException {
         RectangleMethodIntegralCalculator calculator = new RectangleMethodIntegralCalculator(1E-9);
-        long timeBefore = System.currentTimeMillis();
         IntegrationCalculationTask task = new IntegrationCalculationTask(
                 calculator, (x) -> Math.sqrt(1 - x * x), 0, 1);
         Thread calculationThread = new Thread(task);
         calculationThread.start();
+        long timeBefore = System.currentTimeMillis();
         calculationThread.join();
         long timeAfter = System.currentTimeMillis();
         System.out.println("square: " + task.getCalculationResult());
